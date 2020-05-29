e231553971a830123f986bd8fd3f049ceac8886d
David Martinez <davidmtzv14@gmail.com>
2020-05-29 02:41:07 -0500

    changes for deployment to Heroku

--- apps/api/src/environments/environment.prod.ts
+++ apps/api/src/environments/environment.prod.ts
@@ -1,3 +1,19 @@
 export const environment = {
-  production: true
+  production: true,
+  databaseConfig: {
+    type: 'postgres',
+    host: 'localhost',
+    port: 5432,
+    database: 'napho_dev',
+    username: 'davidmartinez',
+    password: 'postgres',
+    synchronize: true
+  },
+  server: {
+    port: 3333
+  },
+  jwt: {
+    secret: 'pqowieurytlaksjdhf',
+    expiresIn: 86400
+  }
 };
--- dist/apps/api/main.js
+++ dist/apps/api/main.js
@@ -296,7 +296,23 @@ exports.UserRepository = UserRepository;
 
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.environment = {
-    production: true
+    production: true,
+    databaseConfig: {
+        type: 'postgres',
+        host: 'localhost',
+        port: 5432,
+        database: 'napho_dev',
+        username: 'davidmartinez',
+        password: 'postgres',
+        synchronize: true
+    },
+    server: {
+        port: 3333
+    },
+    jwt: {
+        secret: 'pqowieurytlaksjdhf',
+        expiresIn: 86400
+    }
 };
 
 
